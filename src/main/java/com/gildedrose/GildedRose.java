f0ee8fb56259bdb665c206c2602cdd2cb2ff109a
mdomasevicius <mdomasevicius@gmail.com>
2018-06-02 16:35:51 +0300

    Refactor quality update into something readable

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,72 @@
 package com.gildedrose;
 
+import java.util.*;
+import java.util.function.Consumer;
+
+import static java.util.Arrays.*;
+import static java.util.Optional.*;
+
+/*
+    Since requirments allow me to only change GildedRose#upadeQuality() I will
+    refrain from changing contract e.g. Item[] to List<Item>
+ */
 class GildedRose {
+
+    private Map<String, Consumer<Item>> itemRules = new HashMap<>();
+
+    {
+        itemRules.put(
+                "Aged Brie",
+                item -> {
+                    if (item.sellIn > 0) {
+                        item.quality += 1;
+                    } else {
+                        item.quality += 2;
+                    }
+
+                    item.quality = item.quality > 50 ? 50 : item.quality;
+
+                    item.sellIn -= 1;
+                });
+
+        itemRules.put("Sulfuras, Hand of Ragnaros", item -> item.sellIn -= 1);
+        itemRules.put("Conjured Mana Cake", item -> {
+            if (item.sellIn > 0) {
+                item.quality -= 2;
+            } else {
+                item.quality -= 4;
+            }
+
+            item.quality = item.quality < 0 ? 0 : item.quality;
+
+            item.sellIn -= 1;
+        });
+        itemRules.put("Backstage passes to a TAFKAL80ETC concert", item -> {
+            if (item.sellIn > 10) {
+                item.quality += 1;
+            } else if (item.sellIn > 5) {
+                item.quality += 2;
+            } else if (item.sellIn > 0) {
+                item.quality += 3;
+            }
+
+            item.quality = item.sellIn < 1 ? 0 : item.quality;
+
+            item.sellIn -= 1;
+        });
+    }
+
+    private Consumer<Item> fallbackRule = item -> {
+        if (item.sellIn > 0) {
+            item.quality -= 1;
+        } else {
+            item.quality -= 2;
+        }
+        item.quality = item.quality < 0 ? 0 : item.quality;
+
+        item.sellIn -= 1;
+    };
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -8,55 +74,8 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
+        stream(items).forEach(item ->
+                ofNullable(itemRules.get(item.name)).orElse(fallbackRule).accept(item));
     }
+
 }
