2db8aa3c0f104d5a6186cb9fa064ff3bfe815d71
mdomasevicius <mdomasevicius@gmail.com>
2018-06-03 10:22:17 +0300

    Only expose read methods

--- src/main/java/com/gildedrose/rest/ItemRepo.java
+++ src/main/java/com/gildedrose/rest/ItemRepo.java
@@ -1,8 +1,33 @@
 package com.gildedrose.rest;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-@RepositoryRestResource(collectionResourceRel = "item", path = "items")
+import java.util.Optional;
+
+@RepositoryRestResource(
+    collectionResourceRel = "item",
+    path = "items",
+    excerptProjection = ItemEntity.ListProjection.class)
 interface ItemRepo extends PagingAndSortingRepository<ItemEntity, Long> {
+
+    @RestResource
+    @Override
+    Optional<ItemEntity> findById(Long aLong);
+
+    @RestResource
+    @Override
+    Iterable<ItemEntity> findAll();
+
+    @RestResource
+    @Override
+    Page<ItemEntity> findAll(Pageable pageable);
+
+    @RestResource
+    @Override
+    Iterable<ItemEntity> findAll(Sort sort);
 }
