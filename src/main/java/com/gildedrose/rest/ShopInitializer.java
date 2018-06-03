7e70e39abba849fcf4306b76801b6490e6c7954b
mdomasevicius <mdomasevicius@gmail.com>
2018-06-03 22:50:24 +0300

    Add conjured item

--- src/main/java/com/gildedrose/rest/ShopInitializer.java
+++ src/main/java/com/gildedrose/rest/ShopInitializer.java
@@ -55,7 +55,8 @@ class ShopInitializer implements InitializingBean {
                     new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                     new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                     new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                    new Item("Conjured Mana Cake", 3, 6)
+                    new Item("Conjured Mana Cake", 3, 6),
+                    new Item("Conjured Bread", 8, 16)
                 });
 
                 LongStream.rangeClosed(1, days).forEach(ignored -> gildedRose.updateQuality());
